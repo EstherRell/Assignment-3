b5bce7cc3503e1f0f9f808aa9437182858f69c16
EstherRell <68958532+EstherRell@users.noreply.github.com>
2020-08-11 10:23:05 +0000

    HW3

---
+++ src/sharptext.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import './css/components.css'
+function Sharptext() {
+    return (
+        <div>
+            <h1 className=''>Marvel's Fearless</h1>
+            <p className='subtitle'>lorem ipsum - The Unbeatable</p>
+            <br />
+            <div className='search-bar'>
+            <input type='search' placeholder='search' />
+            </div>
+            
+        </div>
+    )
+}
+
+export default Sharptext;
